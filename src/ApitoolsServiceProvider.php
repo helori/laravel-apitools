4dbadca4cff6b7fb7b1d438eaf2133f9541f8b07
helori <helori.lanos@gmail.com>
2017-07-19 17:04:01 +0200

    initial commit

---
+++ src/ApitoolsServiceProvider.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Helori\LaravelApitools;
+
+use Illuminate\Support\ServiceProvider;
+
+
+class ApitoolsServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        
+    }
+    
+    public function boot()
+	{
+        
+	}
+}
